694e9a2990070d62c104d915d59bc84946836d41
acc0mplish <70960296+acc0mplish@users.noreply.github.com>
2026-09-02 20:43:59 +0900

    refactor(i18n): remove Chinese database workbench entry copy

--- web/src/views/assets/DatabaseWorkbenchEntry.vue
+++ web/src/views/assets/DatabaseWorkbenchEntry.vue
@@ -3,20 +3,17 @@ import { onMounted, ref } from 'vue'
 import { useRouter } from 'vue-router'
 import { ElMessage } from 'element-plus'
 import { queryAssetDatabaseList } from '../../api/asset'
+import { at } from '../../utils/asset-i18n'
 
 const router = useRouter()
 const loading = ref(true)
 
 async function enterWorkbench() {
   try {
-    const data = await queryAssetDatabaseList({
-      pageNum: 1,
-      pageSize: 500,
-      status: '1'
-    })
+    const data = await queryAssetDatabaseList({ pageNum: 1, pageSize: 500, status: '1' })
     const database = (data.list || []).find((item) => item.connectStatus === 1)
     if (!database) {
-      ElMessage.warning('暂无可用的数据库连接，请先在数据库列表中完成连接配置')
+      ElMessage.warning(at('noDatabaseConnection'))
       await router.replace('/assets/databases')
       return
     }
@@ -31,7 +28,7 @@ onMounted(enterWorkbench)
 
 <template>
   <div v-loading="loading" class="workbench-loading">
-    <span>正在进入 DBMS 工作台...</span>
+    <span>{{ at('enteringDatabaseWorkbench') }}</span>
   </div>
 </template>
 
